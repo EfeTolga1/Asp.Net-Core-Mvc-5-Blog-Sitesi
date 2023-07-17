5fe810eba6a516dde0a788ea5b5dc2d069934ffa
EfeTolga1 <efetol@gmail.com>
2023-07-17 03:19:43 +0300

    Update Statistic1.cs

--- CoreDemo/CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
+++ CoreDemo/CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
@@ -21,7 +21,7 @@ namespace CoreDemo.Areas.Admin.ViewComponents.Statistic
             ViewBag.v3 = c.Comments.Count();
 
             //Alttaki kodu api ile hava durumunu  çekmek için yazdık ama  connectionı çalıştıramadım
-            //string api = "b9a32499a56084cb4a5726b21cce024e";
+            //string api = "----------------------";
             //string connection = "https://api.openweathermap.org/data/2.5/weather?q=istanbul&mode=xml&lang=tr&units=metric&appid=" + api;
             //XDocument document = XDocument.Load(connection);
             //ViewBag.v4 = document.Descendants("temperature").ElementAt(0).Attribute("value").Value;
